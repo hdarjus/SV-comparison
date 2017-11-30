7bb697d9926a94fdda6355723e7746ba3e72afdd
Darjus Hosszejni <darjus.hosszejni@wu.ac.at>
2017-11-30 18:55:17 +0100

    evaluate.R: small corrections

--- evaluate.R
+++ evaluate.R
@@ -7,7 +7,7 @@ library("zoo")
 source("utils.R")
 
 # Read dataset
-everything <- readRDS("phi-05.RDS")
+everything <- readRDS("phi-99.RDS")
 jafit.c <- everything$jafit.c
 stfit.c <- everything$stfit.c
 svfit.c <- everything$svfit.c
@@ -18,7 +18,7 @@ svfit.asis <- everything$svfit.asis
 dat <- everything$data
 n.draws <- NROW(jafit.nc[[1]])
 N <- length(dat$y)
-i.chain <- 3
+i.chain <- 1
 
 # Stan diagnostics
 # Look for dependence between energy and the parameters. They should be independent
@@ -29,8 +29,8 @@ stan_pair(stfit.nc, i.chain)
 pars <- c("phi", "sigma", "mu")
 japar.c <- jafit.c[[i.chain]][, pars]
 japar.nc <- jafit.nc[[i.chain]][, pars]
-stpar.c <- as.matrix(extract(stfit.c, pars, inc_warmup = FALSE, permuted = FALSE)[,i.chain,])[, pars]
-stpar.nc <- as.matrix(extract(stfit.nc, pars, inc_warmup = FALSE, permuted = FALSE)[,i.chain,])[, pars]
+stpar.c <- drop(extract(stfit.c, pars, inc_warmup = FALSE, permuted = FALSE)[,i.chain,])
+stpar.nc <- drop(extract(stfit.nc, pars, inc_warmup = FALSE, permuted = FALSE)[,i.chain,])
 
 plot(zoo(japar.c), screen = 1:3)
 plot(zoo(japar.nc), screen = 1:3)
@@ -48,8 +48,8 @@ plot(svfit.asis)
 as.data.frame(n.draws / rbind(
   JAGS.c = effectiveSize(japar.c),
   JAGS.nc = effectiveSize(japar.nc),
-  Stan.c = sapply(c("phi" = "phi", "sigma" = "sigma", "mu" = "mu"), function (x) effectiveSize(unlist(extract(stfit.c, x, inc_warmup = FALSE, permuted = FALSE)[,i.chain,]))),
-  Stan.nc = sapply(c("phi" = "phi", "sigma" = "sigma", "mu" = "mu"), function (x) effectiveSize(unlist(extract(stfit.nc, x, inc_warmup = FALSE, permuted = FALSE)[,i.chain,]))),
+  Stan.c = sapply(c("phi" = "phi", "sigma" = "sigma", "mu" = "mu"), function (x) effectiveSize(drop(extract(stfit.c, x, inc_warmup = FALSE, permuted = FALSE)[,i.chain,]))),
+  Stan.nc = sapply(c("phi" = "phi", "sigma" = "sigma", "mu" = "mu"), function (x) effectiveSize(drop(extract(stfit.nc, x, inc_warmup = FALSE, permuted = FALSE)[,i.chain,]))),
   stochvol.c = effectiveSize(svfit.c$para),
   stochvol.nc = effectiveSize(svfit.nc$para),
   stochvol.asis = effectiveSize(svfit.asis$para)
@@ -57,15 +57,15 @@ as.data.frame(n.draws / rbind(
 
 # Latent vector plots
 latent.names <- paste0("h[", 1:N, "]")
-jalatent.c <- as.matrix(jafit.c[[i.chain]][, latent.names])
-jalatent.nc <- apply(as.matrix(jafit.nc[[i.chain]][, latent.names]), 2, function (x, mu, sigma) { sigma*x + mu }, japar.nc[, "mu"], japar.nc[, "sigma"])
-stlatent.c <- as.matrix(as.data.frame(extract(stfit.c, pars = latent.names, permuted = FALSE)[,i.chain,]))
-stlatent.nc <- apply(as.matrix(as.data.frame(extract(stfit.nc, pars = latent.names, permuted = FALSE)[,i.chain,])), 2, function (x, mu, sigma) { sigma*x + mu }, stpar.nc[, "mu"], stpar.nc[, "sigma"])
+jalatent.c <- jafit.c[[i.chain]][, latent.names]
+jalatent.nc <- jafit.nc[[i.chain]][, latent.names]
+stlatent.c <- drop(extract(stfit.c, pars = latent.names, permuted = FALSE)[,i.chain,])
+stlatent.nc <- drop(extract(stfit.nc, pars = latent.names, permuted = FALSE)[,i.chain,])
 
-myvolplot(exp(jalatent.c/2), dat)
-myvolplot(exp(jalatent.nc/2), dat)
+myvolplot(exp(as.matrix(jalatent.c)/2), dat)
+myvolplot(exp(0.5 * apply(as.matrix(jalatent.nc), 2, function (x, mu, sigma) { sigma*x + mu }, japar.nc[, "mu"], japar.nc[, "sigma"])), dat)
 myvolplot(exp(stlatent.c/2), dat)
-myvolplot(exp(stlatent.nc/2), dat)
+myvolplot(exp(0.5 * apply(stlatent.nc, 2, function (x, mu, sigma) { sigma*x + mu }, stpar.nc[, "mu"], stpar.nc[, "sigma"])), dat)
 myvolplot(as.matrix(exp(svfit.c$latent/2)), dat)
 myvolplot(as.matrix(exp(svfit.nc$latent/2)), dat)
 myvolplot(as.matrix(exp(svfit.asis$latent/2)), dat)
